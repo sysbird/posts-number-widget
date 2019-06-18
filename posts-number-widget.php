331a49495c56acf42e6faea5b8c36c6f6e986273
Yuko TORIYAMA <koro@sysbird.jp>
2019-06-18 19:29:48 +0900

    Fix 'Deprecated: Function create_function() '  at  PHP Ver7.2.

--- posts-number-widget.php
+++ posts-number-widget.php
@@ -1,50 +1,49 @@
 <?php
 /*
 Plugin Name:Posts Number Widget
-Plugin URI: http://www.sysbird.jp/wptips/posts-number-widget
+Plugin URI: https://sysbird.jp/wptips/posts-number-widget
 Description: The widget display number of posts.
-Version: 1.0
+Version: 1.1
 Author: Sysbird
-Author URI: http://www.sysbird.jp/wptips
+Author URI: https://sysbird.jp/wptips
 License: GPLv2 or later
 Text Domain: posts-number-widget
-Domain Path: /languages/
 */
 
 class Posts_Number_Widget extends WP_Widget {
 
 	function __construct() {
-		$ret = load_plugin_textdomain( 'posts-number-widget', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
-		$widget_ops = array('description' => __('The Widget display number of posts.', 'posts-number-widget'));
+		$ret = load_plugin_textdomain( 'posts-number-widget', false, plugin_basename( dirname( __FILE__ )) . '/languages' );
+		$widget_ops = array('description' => __('The Widget display number of posts.', 'posts-number-widget' ));
 		parent::__construct(
 			false,
-			__('Posts Number', 'posts-number-widget'),
+			__( 'Posts Number', 'posts-number-widget' ),
 			$widget_ops
 		);
 	}
 
-	public function form($instance) {
-		$instance = wp_parse_args( (array) $instance, array( 'title' => '', 'unit' => '' ) );
+	public function form( $instance ) {
+		$instance = wp_parse_args(( array ) $instance, array( 'title' => '', 'unit' => '' ));
 		$title = $instance['title'];
 		$unit = $instance['unit'];
 
 ?>
-		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'posts-number-widget'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></label></p>
-		<p><label for="<?php echo $this->get_field_id('unit'); ?>"><?php _e('Unit:', 'posts-number-widget'); ?> <input class="widefat" id="<?php echo $this->get_field_id('unit'); ?>" name="<?php echo $this->get_field_name('unit'); ?>" type="text" value="<?php echo esc_attr($unit); ?>" /></label></p>
+		<p><label for="<?php echo $this->get_field_id( 'unit' ); ?>"><?php _e('Unit:', 'posts-number-widget'); ?> <input class="widefat" id="<?php echo $this->get_field_id( 'unit' ); ?>" name="<?php echo $this->get_field_name( 'unit' ); ?>" type="text" value="<?php echo esc_attr( $unit ); ?>" /></label></p>
+		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'posts-number-widget' ); ?> <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" /></label></p>
 <?php
 	}
 
 	function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
-		$new_instance = wp_parse_args((array) $new_instance, array( 'title' => '', 'unit' => '' ));
-		$instance['title'] = strip_tags($new_instance['title']);
-		$instance['unit'] = strip_tags($new_instance['unit']);
+		$new_instance = wp_parse_args(( array ) $new_instance, array( 'title' => '', 'unit' => '' ));
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['unit'] = strip_tags( $new_instance['unit'] );
 		return $instance;
 	}
 
-	public function widget($args, $instance) {
+	public function widget( $args, $instance ) {
 
-		extract($args);
+		extract( $args );
 		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
 		$unit = empty( $instance['unit'] ) ? '' : $instance['unit'];
 		$number = wp_count_posts('post','publish')->publish;
@@ -58,4 +57,4 @@ class Posts_Number_Widget extends WP_Widget {
 	}
 }
 
-add_action( 'widgets_init', create_function( '', ' return register_widget("Posts_Number_Widget"); ') );
+add_action( 'widgets_init', function(){register_widget('Posts_Number_Widget' );});
